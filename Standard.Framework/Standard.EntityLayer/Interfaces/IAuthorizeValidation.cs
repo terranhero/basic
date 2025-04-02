84f31ce15248858d6fe9d6e88dd62c83265d01d1
JACKY <tanqiang@live.com>
2025-04-02 08:18:34 +0800

    修改权限验证接口，添加异步验证方法

--- Standard.Framework/Standard.EntityLayer/Interfaces/IAuthorizeValidation.cs
+++ Standard.Framework/Standard.EntityLayer/Interfaces/IAuthorizeValidation.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using STT = System.Threading.Tasks;
 
 namespace Basic.Interfaces
 {
 	/// <summary>表示用户权限的验证</summary>
 	public interface IAuthorizeValidation
 	{
+		/// <summary>获取角色导航菜单</summary>
+		/// <param name="action">表示读取此导航菜单时需要进行特殊处理的操作</param>
+		/// <returns>当前角色的导航菜单</returns>
+		STT.Task<NavigateMenuCollection> GetNavigateMenusAsync(Action<NavigateMenu> action);
+
+		/// <summary>获取角色导航菜单</summary>
+		/// <returns>当前角色的导航菜单</returns>
+		STT.Task<NavigateMenuCollection> GetNavigateMenusAsync();
+
+		/// <summary>检查授权码是否有效</summary>
+		/// <param name="code">The authorization code.</param>
+		/// <returns>如果有效则返回true，否则返回false。</returns>
+		STT.Task<bool> CheckCodeAsync(int code);
+
+		/// <summary>检查区段授权码是否有效(从开始授权码检查到结束授权码，只要有一个条件则授权成功)。</summary>
+		/// <param name="bCode">按钮授权码开始编号</param>
+		/// <param name="eCode">按钮授权码结束编号</param>
+		/// <returns>如果有效则返回true，否则返回false。</returns>
+		STT.Task<bool> CheckCodeAsync(int bCode, int eCode);
+
+		/// <summary>检查一组授权码是否成功。</summary>
+		/// <param name="codes">表示一组授权码</param>
+		/// <returns>这组授权码其中一个检测成功则返回true，所有授权码检测都不成功则返回false。</returns>
+		STT.Task<bool> CheckCodeAsync(params int[] codes);
+
+		/// <summary>获取角色导航菜单</summary>
+		/// <param name="action">表示读取此导航菜单时需要进行特殊处理的操作</param>
+		/// <returns>当前角色的导航菜单</returns>
+		NavigateMenuCollection GetNavigateMenus(Action<NavigateMenu> action);
+
 		/// <summary>获取角色导航菜单</summary>
 		/// <returns>当前角色的导航菜单</returns>
 		NavigateMenuCollection GetNavigateMenus();
