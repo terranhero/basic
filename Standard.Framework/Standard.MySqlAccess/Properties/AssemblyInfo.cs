bb091f1cd543d83ed6c74fa75b4b66c57ba5812b
JACKY <tanqiang@live.com>
2025-04-30 12:20:02 +0800

    【日志管理】修改日志记录器

--- Standard.Framework/Standard.MySqlAccess/Properties/AssemblyInfo.cs
+++ Standard.Framework/Standard.MySqlAccess/Properties/AssemblyInfo.cs
@@ -1,4 +1,3 @@
-﻿[assembly: System.Reflection.AssemblyDescription("GoldSoft Technology MySql Database Access")]
-[assembly: System.Reflection.AssemblyAlgorithmId(System.Configuration.Assemblies.AssemblyHashAlgorithm.MD5)]
+﻿[assembly: System.Reflection.AssemblyAlgorithmId(System.Configuration.Assemblies.AssemblyHashAlgorithm.MD5)]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("4270B4ED-72D5-43F5-960C-67A11529769C")]
