bb6bef8014b0a48e4e322606871ddbe7b2d18c42
JACKY <tanqiang@live.com>
2025-08-01 08:52:11 +0800

    添加Error方法，方便生成Result类

--- Standard.Framework/Standard.DataAccess/DataAccess/AbstractDataContext.cs
+++ Standard.Framework/Standard.DataAccess/DataAccess/AbstractDataContext.cs
@@ -153,5 +153,54 @@ namespace Basic.DataAccess
 			return MessageContext.GetString(converterName, name, args);
 		}
 		#endregion
+
+		#region 获取区域字符串资源
+		/// <summary>
+		/// 返回指定的 System.String 资源的值。
+		/// </summary>
+		/// <param name="name">要获取的资源名。</param>
+		/// <returns> 针对调用方的当前区域性设置而本地化的资源的值。如果不可能有匹配项，则返回 null。</returns>
+		/// <exception cref="System.ArgumentNullException">name 参数为 null。</exception>
+		/// <exception cref="System.InvalidOperationException">指定资源的值不是字符串。</exception>
+		/// <exception cref="System.Resources.MissingManifestResourceException">未找到可用的资源集，并且没有非特定区域性的资源。</exception>
+		protected Result Error(string name)
+		{
+			return new Result(GetString(name));
+		}
+
+		/// <summary>
+		/// 返回指定的 System.String 资源的值，使用指定的参数替换资源中空缺的值
+		/// </summary>
+		/// <param name="name">资源名称</param>
+		/// <param name="args">一个对象数组，其中包含零个或多个要设置格式的对象。</param>
+		/// <returns>为指定区域性本地化的资源的值。如果不可能有最佳匹配，则返回 null。</returns>
+		public Result Error(string name, params object[] args)
+		{
+			return new Result(GetString(name, args));
+		}
+
+		/// <summary>
+		/// 返回指定的 System.String 资源的值，使用指定的参数替换资源中空缺的值
+		/// </summary>
+		/// <param name="converterName">需要指定的转换器名称。</param>
+		/// <param name="name">资源名称</param>
+		/// <returns> 针对调用方的当前区域性设置而本地化的资源的值。如果不可能有匹配项，则返回 null。</returns>
+		public Result Error(string converterName, string name)
+		{
+			return new Result(GetString(converterName, name));
+		}
+
+		/// <summary>
+		/// 返回指定的 System.String 资源的值，使用指定的参数替换资源中空缺的值
+		/// </summary>
+		/// <param name="converterName">需要指定的转换器名称。</param>
+		/// <param name="name">资源名称</param>
+		/// <param name="args">一个对象数组，其中包含零个或多个要设置格式的对象。</param>
+		/// <returns> 针对调用方的当前区域性设置而本地化的资源的值。如果不可能有匹配项，则返回 null。</returns>
+		public Result Error(string converterName, string name, params object[] args)
+		{
+			return new Result(GetString(converterName, name, args));
+		}
+		#endregion
 	}
 }
