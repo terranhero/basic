e7640f93c19e09533e5a90007d868effb6aaf2a2
JACKY <tanqiang@live.com>
2025-08-09 23:32:43 +0800

    日志测试

--- Standard.Test/Standard.DataAccess.Test/LoggerWriterUnitTest.cs
+++ Standard.Test/Standard.DataAccess.Test/LoggerWriterUnitTest.cs
@@ -25,49 +25,58 @@ namespace Standard.DataAccess.Test
 			// Access TestContext properties and methods here. The properties related to the test run are not available.
 		}
 
+		[TestMethod("GetLoggers")]
+		public async Task GetLoggers()
+		{
+			ILoggerWriter writer = LoggerWriter.GetWriter("LOCAL");
+			IPagination<LoggerEntity> logs = await writer.Storage.GetLoggingsAsync(new Guid("EE7F553B-3541-4354-A451-D2E74D9C64CC"));
+			string text = System.Text.Json.JsonSerializer.Serialize(logs);
+			Console.WriteLine(text);
+		}
+
 		[TestMethod("TestInformationAsync")]
 		public async Task TestInformationAsync()
 		{
-			//ManualResetEvent resetEvent = new ManualResetEvent(false);
-
 			ILoggerWriter writer = LoggerWriter.GetWriter("LOCAL");
-
-			await writer.InformationAsync("LoggerWriter", "InformationAsync", null, "Visual Studio 2022", "这是日志测试");
-			//// 等待 BackgroundWorker 完成（或超时）
-			//bool completed = resetEvent.WaitOne(1000 * 10); // 或指定超时时间，如 5000ms
-		}
-
-		[TestMethod("TestInformationChineseAsync")]
-		public async Task TestInformationChineseAsync()
-		{
-			ILoggerWriter writer = LoggerWriter.GetWriter("LOCAL");
-			await writer.InformationAsync("测试控制器", "测试方法长度而已", null, "Visual Studio 2022", "这是日志测试");
+			for (int index = 1; index <= 5000; index++)
+			{
+				await writer.InformationAsync("LoggerWriter", "InformationAsync", null, "Visual Studio 2022", "这是日志测试");
+			}
+			//return Task.Delay(1000);
 		}
 
 		[TestMethod("TestErrorAsync")]
 		public async Task TestErrorAsync()
 		{
 			ILoggerWriter writer = LoggerWriter.GetWriter("LOCAL");
-			await writer.ErrorAsync("LoggerWriter", "ErrorAsync", null, "Visual Studio 2022", "这是日志测试");
+			for (int index = 1; index <= 5000; index++)
+			{
+				await writer.ErrorAsync("LoggerWriter", "ErrorAsync", null, "Visual Studio 2022", "这是日志测试");
+			}
+			//return Task.Delay(1000);
 		}
 
 		[TestMethod("TestDebugAsync")]
 		public async Task TestDebugAsync()
 		{
 			ILoggerWriter writer = LoggerWriter.GetWriter("LOCAL");
-			await writer.DebugAsync("LoggerWriter", "DebugAsync", null, "Visual Studio 2022", "这是日志测试");
+			for (int index = 1; index <= 5000; index++)
+			{
+				await writer.DebugAsync("LoggerWriter", "DebugAsync", null, "Visual Studio 2022", "这是日志测试");
+			}
+			//await Task.Delay(1000);
 		}
 
 		[TestMethod("TestWarnAsync")]
 		public async Task TestWarnAsync()
 		{
 			ILoggerWriter writer = LoggerWriter.GetWriter("LOCAL");
-			for (int index = 0; index <= 2000; index++)
+			for (int index = 1; index <= 5000; index++)
 			{
-				await writer.WarningAsync("控制器", "WarningAsync", null, "Visual Studio 2022", "这是日志测试");
-				//await writer.WarningAsync("控制器", "WarningAsync", null, "Visual Studio 2022", "这是日志测试");
+				await writer.WarningAsync("LoggerWriter", "WarningAsync", null, "Visual Studio 2022", $"这是日志测试{index}");
 			}
-			await Task.Delay(3000);
 		}
+		[TestMethod("ZWarnAsync,等待日志队列")]
+		public async Task Waiting() { await Task.Delay(10000); }
 	}
 }
