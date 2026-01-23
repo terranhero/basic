e94572408af5c4c66b715b7cd50042a09ca284f9
TAN <tanqiang@goldsofttech.com>
2026-01-23 10:31:41 +0800

    为LoggerOptions 添加特性

--- Standard.Framework/Standard.EntityLayer/Loggers/LoggerOptions.cs
+++ Standard.Framework/Standard.EntityLayer/Loggers/LoggerOptions.cs
@@ -1,10 +1,12 @@
-﻿using Basic.Enums;
+﻿using Basic.EntityLayer;
+using Basic.Enums;
 
 namespace Basic.Loggers
 {
 	/// <summary>
 	/// 自定义日志配置信息。
 	/// </summary>
+	[global::Basic.EntityLayer.GroupNameAttribute("Logger")]
 	public sealed class LoggerOptions
 	{
 		private static LoggerOptions _default = new LoggerOptions();
@@ -18,24 +20,30 @@ namespace Basic.Loggers
 		private LoggerOptions() { }
 
 		/// <summary>系统日志表</summary>
+		[WebDisplayAttribute("Logger_TableName")]
 		public string TableName { get; set; } = "SYS_EVENTLOGGER";
 
 		/// <summary>文件型日志保存模式(按天/按周/按月)。</summary>
+		[WebDisplayAttribute("Logger_Mode")]
 		public CycleMode Mode { get; set; } = CycleMode.Weekly;
 
 		/// <summary>消息日志保存方式</summary>
+		[WebDisplayAttribute("Logger_LogLevel")]
 		public LogLevelOption Information { get { return _information; } }
 		private LogLevelOption _information = new LogLevelOption(LogLevel.Information, LogSaveType.DataBase, true);
 
 		/// <summary>警告日志保存方式</summary>
+		[WebDisplayAttribute("Logger_LogLevel")]
 		public LogLevelOption Warning { get { return _warning; } }
 		private LogLevelOption _warning = new LogLevelOption(LogLevel.Warning, LogSaveType.DataBase, true);
 
 		/// <summary>错误日志保存方式</summary>
+		[WebDisplayAttribute("Logger_LogLevel")]
 		public LogLevelOption Error { get { return _error; } }
 		private LogLevelOption _error = new LogLevelOption(LogLevel.Error, LogSaveType.DataBase, true);
 
 		/// <summary>调试日志保存方式</summary>
+		[WebDisplayAttribute("Logger_LogLevel")]
 		public LogLevelOption Debug { get { return _debug; } }
 		private LogLevelOption _debug = new LogLevelOption(LogLevel.Debug, LogSaveType.LocalFile, false);
 
@@ -46,7 +54,8 @@ namespace Basic.Loggers
 
 	/// <summary>特定日志级别存储方式</summary>
 	[System.Diagnostics.DebuggerDisplay("SaveType = {SaveType}, Enabled = {Enabled}")]
-	public sealed class LogLevelOption
+    [global::Basic.EntityLayer.GroupNameAttribute("Logger")]
+    public sealed class LogLevelOption
 	{
 		/// <summary>
 		/// 初始化 LogLevelOption 类实例。
@@ -67,14 +76,17 @@ namespace Basic.Loggers
 		}
 
 		/// <summary>日志级别。</summary>
+		[WebDisplayAttribute("Logger_LogLevel")]
 		public LogLevel LogLevel { get; private set; }
 
 		/// <summary>日志保存类型</summary>
+		[WebDisplayAttribute("Logger_SaveType")]
 		public LogSaveType SaveType { get; set; }
 
 		/// <summary>
 		/// 该级别日志配置信息是否有效。
 		/// </summary>
+		[WebDisplayAttribute("Logger_Enabled")]
 		public bool Enabled { get; set; }
 
 		///// <summary>
